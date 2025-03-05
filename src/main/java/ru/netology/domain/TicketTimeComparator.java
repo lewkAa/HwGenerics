0ba7a69d08e8d97e6c56b5c9137fc982745bcd00
lewkAa <tihonovtan@gmail.com>
2025-03-05 13:22:19 +0300

    Add code and tests

---
+++ src/main/java/ru/netology/domain/TicketTimeComparator.java
@@ -0,0 +1,17 @@
+package ru.netology.domain;
+
+import java.util.Comparator;
+
+public class TicketTimeComparator implements Comparator<Ticket> {
+
+    @Override
+    public int compare(Ticket o1, Ticket o2) {
+        if (o1.flightTime() < o2.flightTime()){
+            return -1;
+        } else if (o1.flightTime() > o2.flightTime()) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+}
---
+++ src/test/java/ru/netology/test/AviaSoulsTest.java
@@ -0,0 +1,47 @@
+package ru.netology.test;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import ru.netology.domain.*;
+
+import java.util.Comparator;
+
+public class AviaSoulsTest {
+
+    Ticket ticket1 = new Ticket("MSC", "SPB", 3500, 1630, 1750);
+    Ticket ticket2 = new Ticket("MSC", "SPB", 2700, 630, 800);
+    Ticket ticket3 = new Ticket("MSC", "SPB", 2000, 200, 350);
+    Ticket ticket4 = new Ticket("SMR", "ORN", 1800, 1500, 1620);
+    Ticket ticket5 = new Ticket("SMR", "ORN", 2000, 1100, 1230);
+    Ticket ticket6 = new Ticket("SPB", "KLG", 1200, 1500, 1600);
+    Ticket ticket7 = new Ticket("NVSB", "VDVS", 1800, 1520, 1620);
+
+
+    @Test
+    void shouldSearch() {
+        AviaSouls service = new AviaSouls();
+        service.add(ticket1, ticket2, ticket3, ticket4, ticket5, ticket6, ticket7);
+
+        Ticket[] expected = {ticket3, ticket2, ticket1};
+        Ticket[] actual = service.search("MSC", "SPB");
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    void shouldSearchAndSort() {
+        AviaSouls service = new AviaSouls();
+        service.add(ticket1, ticket2, ticket3, ticket4, ticket5, ticket6, ticket7);
+
+        Ticket[] expected = {ticket1, ticket3, ticket2};
+
+        Comparator<Ticket> flightTimeComparator = new TicketTimeComparator();
+
+        Ticket[] actual = service.searchAndSort("MSC", "SPB", flightTimeComparator);
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+}
